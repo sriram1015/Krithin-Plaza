e1646a220a85a670784b721760ada7f2c513d05b
sriram1015 <127611008+sriram1015@users.noreply.github.com>
2025-02-03 15:59:49 +0530

    userdashboaed updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,21 +4,29 @@ import Preloader from "./preloaderpage.jsx";
 import LoginPage from "./component/auth/login.jsx";
 import AdminPage from "./component/auth/adminsignin.jsx";
 import SignupPage from "./component/auth/signup.jsx";
+import Home from './component/Page/Home.jsx';
+import UserDashboard from './component/Page/UserDashboard'; // Import the UserDashboard
+import Setting from './component/Page/Setting'; // Import the Setting
 
 const App = () => {
   const [loading, setLoading] = useState(true);
 
   return (
     <>
+      
       {loading ? (
         <Preloader onComplete={() => setLoading(false)} />
       ) : (
         <Router>
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/admin" element={<AdminPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-          </Routes>
+          <UserDashboard> 
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/admin" element={<AdminPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/setting" element={<Setting />} /> 
+            </Routes>
+          </UserDashboard>
         </Router>
       )}
     </>
---
+++ client/src/component/Page/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Home Page</h1>
+      <p>Welcome to the homepage!</p>
+    </div>
+  );
+};
+
+export default HomePage;
