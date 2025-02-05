0e1aec148f42833896e54f2723db94b63695d33e
Sriram Venkatachalpathy <127611008+sriram1015@users.noreply.github.com>
2025-02-05 14:34:47 +0530

    user update

---
+++ client/client/src/component/Page/User/Setting.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Setting = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Home Page</h1>
+      <p>Welcome to the Setting!</p>
+    </div>
+  );
+};
+
+export default Setting;
+
